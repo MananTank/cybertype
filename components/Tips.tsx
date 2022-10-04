b6ca6e06c57131a2185bb1722574ea5e0e38cad8
Manan Tank <manantankm@gmail.com>
2022-10-04 20:26:12 +0530

    update tips

--- components/Tips.tsx
+++ components/Tips.tsx
@@ -12,12 +12,12 @@ export function Tips(props: Props) {
 			<div className={styles.tips}>
 				<h2 aria-label='Tips'>{QuestionIcon}</h2>
 				<p>
-					Hit <kbd>Enter</kbd> OR click on {ResetIcon} to Reset for staring a new session.
+					Hit <kbd>Enter</kbd> OR click on {ResetIcon} to Reset for starting a new typing session.
 				</p>
 
 				<p>
-					Hit <kbd>option + delete</kbd> in MacOS OR <kbd>alt + backspace</kbd> in Windows to go
-					back one word
+					Hit <kbd>option + delete</kbd> in Mac OR <kbd>alt + backspace</kbd> in Windows to go back
+					one word instead of a single character
 				</p>
 			</div>
 		</>
