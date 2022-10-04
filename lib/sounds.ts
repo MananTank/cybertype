0e93735b48342d38ea539ff542d7c97e6d579310
Manan Tank <manantankm@gmail.com>
2022-10-04 23:45:09 +0530

    lower the click sounds

--- lib/sounds.ts
+++ lib/sounds.ts
@@ -6,12 +6,12 @@ export type Sounds = {
 };
 
 export function getSounds(): Sounds {
-	const click1 = new Howl({ src: '/sounds/click1.aac', volume: 1 });
-	const click2 = new Howl({ src: '/sounds/click2.aac', volume: 1 });
-	const click3 = new Howl({ src: '/sounds/click3.aac', volume: 1 });
-	const click4 = new Howl({ src: '/sounds/click4.aac', volume: 1 });
-	const click5 = new Howl({ src: '/sounds/click5.aac', volume: 1 });
-	const click6 = new Howl({ src: '/sounds/click6.aac', volume: 1 });
+	const click1 = new Howl({ src: '/sounds/click1.aac', volume: 0.3 });
+	const click2 = new Howl({ src: '/sounds/click2.aac', volume: 0.3 });
+	const click3 = new Howl({ src: '/sounds/click3.aac', volume: 0.3 });
+	const click4 = new Howl({ src: '/sounds/click4.aac', volume: 0.3 });
+	const click5 = new Howl({ src: '/sounds/click5.aac', volume: 0.3 });
+	const click6 = new Howl({ src: '/sounds/click6.aac', volume: 0.3 });
 
 	const clicks = [click1, click2, click3, click4, click5, click6];
 
@@ -20,6 +20,6 @@ export function getSounds(): Sounds {
 			const i = Math.round(Math.random() * (clicks.length - 1));
 			clicks[i].play();
 		},
-		error: new Howl({ src: '/sounds/miss-click.aac', volume: 0.2 }),
+		error: new Howl({ src: '/sounds/miss-click.aac', volume: 0.1 }),
 	};
 }
