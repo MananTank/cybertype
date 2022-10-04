6823510fc38a645c4d651a7cf23cfe794ae99bfa
Manan Tank <manantankm@gmail.com>
2022-10-04 20:51:28 +0530

    add dimension on space icon svg

--- components/icons.tsx
+++ components/icons.tsx
@@ -105,7 +105,13 @@ export const ChevronIcon = (
 );
 
 export const SpaceIcon = (
-	<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill={fill}>
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 24 24'
+		height={iconSize}
+		width={iconSize}
+		fill={fill}
+	>
 		<path d='M0 0h24v24H0V0z' fill='none' />
 		<path d='M18 9v4H6V9H4v6h16V9z' />
 	</svg>
