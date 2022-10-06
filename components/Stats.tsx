f455067eed14ef3fa2ca778502c49b3ee1955349
Manan Tank <manantankm@gmail.com>
2022-10-06 16:03:26 +0530

    start Accuracy with 0 instead of 100

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -11,7 +11,7 @@ export function Stats(props: StatsProps) {
 	const { timeTaken, errors, charsTyped } = props;
 	const wpmSpeed = getSpeed(charsTyped, timeTaken * 5);
 	const errorRate = charsTyped === 0 ? 0 : (errors * 100) / charsTyped;
-	const accuracy = Math.max(Math.round(100 - errorRate), 0);
+	const accuracy = charsTyped === 0 ? 0 : Math.max(Math.round(100 - errorRate), 0);
 
 	return (
 		<div className={styles.statsContainer}>
