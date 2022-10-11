531eedb8ea569b3b085aaa32b68feb4b9df8f786
Manan Tank <manantankm@gmail.com>
2022-10-11 11:04:20 +0530

    add next.js eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended'
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
